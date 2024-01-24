889dec03f864b00a50bc2b07c1195caa38a2c026
stantonik <stanleyy.arnaud@gmail.com>
2024-01-24 16:38:50 +0100

    Setup basic Django project

---
+++ Reveal/Reveal_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RevealAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Reveal_app'
---
+++ Reveal/Reveal_app/urls.py
@@ -0,0 +1,9 @@
+from Reveal.urls import path
+from . import views
+
+urlpatterns = [
+        path("", views.home, name="home"),
+        path("home", views.home, name="home"),
+        # path("contact", views.contact, name="contact"),
+        # path("about", views.about, name="about"),
+        ]
